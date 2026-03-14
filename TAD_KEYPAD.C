ed3c5eb3f098830f5a4090668bae40cc2d81a0bc
arnau <arnauricaart@gmail.com>
2026-03-14 17:51:24 +0100

    keypad

--- TAD_KEYPAD.C
+++ TAD_KEYPAD.C
@@ -15,7 +15,7 @@ static unsigned char columna = 0;
 static unsigned char fila = 0;
 static unsigned char posicioTecla = 0;
 
-static unsigned char teclaPendiente = KEYPAD_NINGUNA; // 20 = ninguna
+static unsigned char teclaPendiente = KEYPAD_NINGUNA;
 static unsigned char primerCiclo = 0;
 
 static unsigned char m;
@@ -28,18 +28,18 @@ static const unsigned char keypad[4][3] = {
 };
 
 static const unsigned char keypadLectura[12][5] = {
-    {'0',' ','0',' ','0'}, // 0 (2)
-    {'1','1','1','1','1'}, // 1 (1)
-    {'2','A','B','C','C'}, // 2 (3)
+    {'0',' ','0',' ','0'}, // 0
+    {'1','1','1','1','1'}, // 1
+    {'2','A','B','C','C'}, // 2
     {'3','D','E','F','F'}, // 3
     {'4','G','H','I','I'}, // 4
     {'5','J','K','L','L'}, // 5
     {'6','M','N','O','O'}, // 6
-    {'7','P','Q','R','S'}, // 7 (4)
+    {'7','P','Q','R','S'}, // 7
     {'8','T','U','V','V'}, // 8
-    {'9','W','X','Y','Z'}, // 9 (4)
-    {'*','*','*','*','*'}, // 10 (1)
-    {'#','#','#','#','#'}  // 11 (1)
+    {'9','W','X','Y','Z'}, // 9
+    {' ',' ',' ',' ',' '}, // 10 = *
+    {' ',' ',' ',' ',' '}  // 11 = #
 };
 
 static void barridoPorts(unsigned char col) {
@@ -47,16 +47,16 @@ static void barridoPorts(unsigned char col) {
     LATBbits.LATB1 = 1;
     LATBbits.LATB2 = 1;
 
-    if(col == 0) LATBbits.LATB7 = 0;
-    if(col == 1) LATBbits.LATB1 = 0;
-    if(col == 2) LATBbits.LATB2 = 0;
+    if (col == 0) LATBbits.LATB7 = 0;
+    if (col == 1) LATBbits.LATB1 = 0;
+    if (col == 2) LATBbits.LATB2 = 0;
 }
 
 static unsigned char getFila(void) {
-    if(PORTBbits.RB3 == 0) return 0;
-    if(PORTBbits.RB4 == 0) return 1;
-    if(PORTBbits.RB5 == 0) return 2;
-    return 3; // RB6
+    if (PORTBbits.RB3 == 0) return 0;
+    if (PORTBbits.RB4 == 0) return 1;
+    if (PORTBbits.RB5 == 0) return 2;
+    return 3;
 }
 
 static unsigned char NumTeclaKeypad(unsigned char col, unsigned char fil) {
@@ -68,27 +68,27 @@ static char traduccioKeypad(unsigned char clicks, unsigned char tecla) {
     return keypadLectura[tecla][clicks];
 }
 
-static unsigned char maxClicks(unsigned char tecla){
-    if (tecla == 1 || tecla == 10 || tecla == 11) return 1; // 1,*,#
-    if (tecla == 0) return 2;                               // 0 
-    if (tecla == 7 || tecla == 9) return 5;                 // 7 i 9
-    return 4;                                               // 2-6-8
+static unsigned char maxClicks(unsigned char tecla) {
+    if (tecla == 1 || tecla == 10 || tecla == 11) return 1; // 1, *, #
+    if (tecla == 0) return 2;                               // 0
+    if (tecla == 7 || tecla == 9) return 5;                 // 7, 9
+    return 4;                                               // 2,3,4,5,6,8
 }
 
-static void resetSMSState(void){
+static void resetSMSState(void) {
     numClicksSMS = 0;
     primerCiclo = 0;
 }
 
-static void commitPendiente(void){
+static void commitPendiente(void) {
     if (teclaPendiente == KEYPAD_NINGUNA) return;
+
     SIO_sendChar(traduccioKeypad(numClicksSMS, teclaPendiente));
     teclaPendiente = KEYPAD_NINGUNA;
     resetSMSState();
 }
 
-void KEY_Init(void){
-
+void KEY_Init(void) {
     ADCON1bits.PCFG = 0x0F;
 
     TRISBbits.TRISB7 = 0;
@@ -114,11 +114,11 @@ void KEY_Init(void){
     resetSMSState();
 }
 
-void KEY_Motor(void){
-    switch(estat){
+void KEY_Motor(void) {
+    switch (estat) {
         case 0:
             barridoPorts(columna = 0);
-            if (primerCiclo && TI_GetTics(timerSMS) >= 500) { 
+            if (primerCiclo && TI_GetTics(timerSMS) >= 500) {
                 commitPendiente();
                 estat = 0;
             } else if (TECLA_PREMUDA()) {
@@ -155,7 +155,7 @@ void KEY_Motor(void){
             }
             break;
 
-        case 3: 
+        case 3:
             if (TI_GetTics(timerRebotes) >= 9) {
                 fila = getFila();
                 posicioTecla = NumTeclaKeypad(columna, fila);
@@ -165,7 +165,7 @@ void KEY_Motor(void){
             }
             break;
 
-        case 4: 
+        case 4:
             if (PORTBbits.RB3 && PORTBbits.RB4 && PORTBbits.RB5 && PORTBbits.RB6) {
                 TI_ResetTics(timerRebotes);
                 estat = 5;
@@ -174,7 +174,7 @@ void KEY_Motor(void){
             }
             break;
 
-        case 5: 
+        case 5:
             if (TI_GetTics(timerRebotes) >= 9) {
                 estat = 6;
             } else {
@@ -182,59 +182,50 @@ void KEY_Motor(void){
             }
             break;
 
-        case 6: {
-
+        case 6:
             if (posicioTecla == KEYPAD_NINGUNA) {
                 estat = 0;
                 break;
             }
 
-            if (teclaPendiente != KEYPAD_NINGUNA && teclaPendiente != posicioTecla) {
-                SIO_sendChar(traduccioKeypad(numClicksSMS, teclaPendiente));
-
-                teclaPendiente = posicioTecla;
-                numClicksSMS = 0;
-                TI_ResetTics(timerSMS);
-                primerCiclo = 1;
-
-                if (maxClicks(teclaPendiente) == 1) {
-                    SIO_sendChar(traduccioKeypad(0, teclaPendiente));
-                    teclaPendiente = KEYPAD_NINGUNA;
-                    resetSMSState();
-                }
-                estat = 0;
-                break;
-            }
-
+            // No hay tecla pendiente: guardamos la nueva
             if (teclaPendiente == KEYPAD_NINGUNA) {
                 teclaPendiente = posicioTecla;
                 numClicksSMS = 0;
                 TI_ResetTics(timerSMS);
                 primerCiclo = 1;
-
-                if (maxClicks(teclaPendiente) == 1) {
-                    SIO_sendChar(traduccioKeypad(0, teclaPendiente));
-                    teclaPendiente = KEYPAD_NINGUNA;
-                    resetSMSState();
-                }
                 estat = 0;
                 break;
             }
 
+            // Si es una tecla distinta, primero confirmamos la anterior
+            // y dejamos la nueva pendiente para la siguiente vuelta
+            if (teclaPendiente != posicioTecla) {
+                commitPendiente();
+
+                teclaPendiente = posicioTecla;
+                numClicksSMS = 0;
+                TI_ResetTics(timerSMS);
+                primerCiclo = 1;
+                estat = 0;
+                break;
+            }
+
+            // Si es la misma tecla, avanzamos el ciclo SMS
             m = maxClicks(teclaPendiente);
             if (m > 1) {
                 numClicksSMS = (numClicksSMS + 1) % m;
                 TI_ResetTics(timerSMS);
                 primerCiclo = 1;
             } else {
-                SIO_sendChar(traduccioKeypad(0, teclaPendiente));
-                teclaPendiente = KEYPAD_NINGUNA;
-                resetSMSState();
+                // para 1, * y # no hay ciclo, se queda pendiente igualmente
+                // y se enviará por timeout o al cambiar de tecla
+                TI_ResetTics(timerSMS);
+                primerCiclo = 1;
             }
 
             estat = 0;
             break;
-        }
 
         default:
             estat = 0;
